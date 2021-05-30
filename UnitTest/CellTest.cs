50151facaa31c93de8757c782867eb9ec50feef2
thowe <tho.you@web.de>
2021-05-30 15:45:48 +0200

    Unit Test Framework + Cell Test

---
+++ UnitTest/CellTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GameOfLife;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class CellTest
+    {
+        [TestMethod]
+        public void TestCostructor()
+        {
+            var cell = new Cell(false, 1, 3);
+            Assert.IsFalse(cell.Alive);
+            Assert.AreEqual(cell.Row, 1);
+            Assert.AreEqual(cell.Column, 3);
+        }
+    }
+}
---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("efbc0d85-5525-4185-bfd7-1290980cfbbe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
